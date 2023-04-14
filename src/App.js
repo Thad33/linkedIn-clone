0481229a30868c527a5a71076dd5bc379f4e4659
Thaddaeus <thaddaeusotieno@gmail.com>
2023-04-14 19:58:12 +0300

    /

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets build linkedin clone</h1>
+
+      {/* <Header /> */}
+      <Header />
+      {/* <App body /> */}
+         {/* <Sidebar /> */}
+         {/* <Feed /> */}
+         {/* <Widgets /> */}
+      {/* <Header /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firbase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBc6Dk6sVELMGIaCvvhKJ7e94qouk6jrm0",
+    authDomain: "link-48f98.firebaseapp.com",
+    projectId: "link-48f98",
+    storageBucket: "link-48f98.appspot.com",
+    messagingSenderId: "637555078631",
+    appId: "1:637555078631:web:3a3b296d01b91082277942",
+    measurementId: "G-53GZ14R3N0"
+  };
